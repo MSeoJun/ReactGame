745711591a08fc7237384970ce6385b5d112750e
kali1402 <amcopeew68@naver.com>
2020-10-07 13:39:57 +0900

    단어 설명 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { getWord } from "./Api/Api";
 
 function App() {
   const [inputData, setInputData] = useState("");
   const [word, setword] = useState("나무");
+  const [results, setresults] = useState("");
 
   const onChange = (e) => {
     setInputData(e.target.value);
@@ -12,6 +13,9 @@ function App() {
   const onClick = async () => {
     if (inputData[0] === word[word.length - 1]) {
       const result = await getWord(inputData);
+      setresults(result[0].description.split(".")[0]);
+      console.log('-------results-------');
+      console.log(result[0].description);
       if (result.length === 0) {
         alert("없는단어");
       } else {
@@ -29,6 +33,7 @@ function App() {
         <input value={inputData} placeholder="단어를 입력하세요." onChange={onChange} />
         <button onClick={onClick}>확인</button>
         <p>{word}</p>
+        <p>{results}</p>
       </div>
     </>
   );
